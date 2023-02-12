114cba89b21d8e3c99439a750f01d3c559063132
Ujwal19k <ujwalkatariya17@gmail.com>
2023-02-12 23:12:48 +0530

    first prog

---
+++ Assignment questions/02. First java program/Even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Write a program to print whether a number is even or odd, also take input from the user.
+public class Even{
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int no = n.nextInt();
+        if(no%2==0){
+            System.out.println(no + " is even");
+        }
+        else{
+            System.out.println(no + " is odd");
+        }
+    }
+}
