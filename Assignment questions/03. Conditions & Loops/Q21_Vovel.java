31fb30d6e402c8696a5961e5a756e75f191b8fc2
Ujwal19k <ujwalkatariya17@gmail.com>
2023-03-10 15:37:39 +0530

    done confitionals

--- Assignment questions/03. Conditions & Loops/Q21_Vovel.java
+++ Assignment questions/03. Conditions & Loops/Q21_Vovel.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 
 public class Q21_Vovel {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String word=sc.next();
-        
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
     }
 }
