42fa064426e5a4eda243bc1b6030a20a7925798b
Ujwal19k <ujwalkatariya17@gmail.com>
2023-03-15 19:44:51 +0530

    added new

--- Assignment questions/04. Arrays/Q05_Shuffle.java
+++ Assignment questions/04. Arrays/Q05_Shuffle.java
@@ -1,3 +1,15 @@
-public class Q05_Shuffle {
-    
+//1470. Shuffle the Array
+//https://leetcode.com/problems/shuffle-the-array/description/
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] narr = new int[2*n];
+        int j=0;  //j is used for sequential selection of the index in the nums
+        for(int i=0;i<nums.length;i+=2){
+            narr[i]=nums[j++]; //for index 0,2,4
+        }
+        for(int i=1;i<nums.length;i+=2){
+            narr[i]=nums[j++];
+         }
+        return narr;
+    }
 }
