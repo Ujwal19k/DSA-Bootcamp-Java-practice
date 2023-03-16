90a627e849b7f241268190f59a38ef70a9c5959f
Ujwal19k <ujwalkatariya17@gmail.com>
2023-03-16 23:21:50 +0530

    arrays

---
+++ Assignment questions/04. Arrays/Q07_GoodPairs.java
@@ -0,0 +1,21 @@
+// /1512. Number of Good Pairs
+//https://leetcode.com/problems/number-of-good-pairs/description/
+
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        int j=0;
+        for(int i=0; i<nums.length;i++){
+            for(j=i+1;j<nums.length-1;j++){     //-1 because i should iterate till second last element because the last element will be covered by j
+                //for every iteration i there should be another iteration starting from i+ in the same array
+                if(nums[i]==nums[j]){
+                count++;
+            }
+            }
+        }
+        return count;
+        
+    }
+}
+
