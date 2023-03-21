3344c43e8c345700b2d91857cb2e364efede56ef
Ujwal19k <ujwalkatariya17@gmail.com>
2023-03-21 22:05:59 +0530

    pangram

--- Assignment questions/04. Arrays/Q10_Pangram.java
+++ Assignment questions/04. Arrays/Q10_Pangram.java
@@ -1,25 +1,53 @@
 //1832. Check if the Sentence Is Pangram
 //https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+//YT: https://www.youtube.com/watch?v=yd9IHmKPdE4&t=989s
 
 
-//this is wrong solution 
-//we'll have to use different concept like hashmap
-public class Q10_Pangram {
+class Q10_Pangram{
     public static void main(String[] args) {
-        String str="ujwal";
-        int count=0;
-        for(int i=0;i<str.length();i++){
-            int num=str.charAt(i);
-            if(!(num>=97 && num<=122)){
-                count++;
-            }
-        }
-        if(count==0){
-            System.out.println("The string is Pangram");
-        }else{
-            System.out.println("The string is not Pangram");
-        }
+        boolean ans = checkPangram("thequickbrownfoxjumpsoverthelazydog"); //true
+        //boolean ans = checkPangram("thequickbrownxjumpsoverthelazydog");    //false
+        System.out.println(ans);
+
+        
         
     }
-    
+
+    public static boolean checkPangram(String sentence){
+        boolean[] arr=new boolean[26];
+        //this will store the record of the letters present in the sentence
+        // true if the letter present and false if not
+        //ASCII: a-97 and z-122
+        for(int i=0; i<sentence.length();i++){     
+            arr[sentence.charAt(i)-97]=true;
+        } 
+        for(int i=0; i<26;i++){     //this will traverse through the array arr
+            if(arr[i]==false){
+                return false;
+            }
+        }
+        
+        return true;
+
+    }
 }
+
+
+
+/*class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] arr=new boolean[26]; //this will store the record of the letters present in the sentence
+        // true if the letter present and false if not
+        //ASCII: a-97 and z-122
+
+        for(int i=0; i<sentence.length();i++){
+            arr[sentence.charAt(i)-97]=true;
+        } 
+        for(int i=0;i<26;i++){
+            if(arr[i]==false){
+                return false;
+            }
+        }
+        return true;
+    }
+} */
