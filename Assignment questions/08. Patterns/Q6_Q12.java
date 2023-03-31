cfd2afec573d22d0177bcb7a4039c53198c8e545
Ujwal19k <ujwalkatariya17@gmail.com>
2023-03-31 20:51:20 +0530

    new

--- Assignment questions/08. Patterns/Q6_Q12.java
+++ Assignment questions/08. Patterns/Q6_Q12.java
@@ -1,6 +1,25 @@
 public class Q6_Q12 {
     public static void main(String[] args) {
-        pattern6(5);
+        //pattern6(5);
+        pattern7(5);
+    }
+
+    private static void pattern7(int n) {
+    //    *****
+    //     ****
+    //      ***
+    //       **
+    //        *
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(j>=n-i+1){       //5-1+1=5....5-2+1=4 .....5-5+1=1
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+        }System.out.println();
+    }
+
     }
 
     private static void pattern6(int n) {
