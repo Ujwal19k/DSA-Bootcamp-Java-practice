98979d464f118e5d25a154ff79dda1961b579c62
Ujwal19k <ujwalkatariya17@gmail.com>
2023-06-04 23:22:23 +0530

    new

---
+++ Assignment questions/04. Arrays/Q18_ArrayFormInteger.java
@@ -0,0 +1,18 @@
+// 989. Add to Array-Form of Integer
+// /https://leetcode.com/problems/add-to-array-form-of-integer/description/
+
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        LinkedList<Integer> listt = new LinkedList<Integer>();
+        int n = num.length - 1;
+        while (n >= 0 || k != 0) {
+            if (n >= 0) {
+                k += num[n];
+                n--;
+            }
+            listt.addFirst(k % 10);
+            k /= 10;
+        }
+        return listt;
+    }
+}
